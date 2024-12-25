03b761897db6f3400fb986f4a019174ee7fba488
RaviKiran-incrivelsoft <Ravikiran@incrivelsoft.com>
2024-12-25 11:40:33 +0530

    'email'

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ require('dotenv').config();
 const connectDB = require('./db');
 const campaignRoutes = require('./routes/campaignRoutes');
 const userRoutes = require('./routes/userRoutes');
+const emailRoutes = require('./routes/emailRoutes');
 
 const app = express()
 
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
+app.use('/emails', emailRoutes);
 app.use('/campaigns', campaignRoutes);
 app.use('/users', userRoutes);
 
--- models/Campaign.js
+++ models/Campaign.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid');
-
-const campaignSchema = new mongoose.Schema({
-	campaignName: {
-		type: String,
-		required: true,
-	},
-	campaignDescription: {
-		type: String,
-		required: true
-	},
-	mediaURL: {
-		type: String,
-		required: true
-	},
-	status: {
-		type: String,
-		enum: ["active", "inactive"],
-		default: 'active',
-	},
-	userId: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "UserDetails"
-	}
-});
-
-module.exports = mongoose.model('CampaignDetails', campaignSchema);
-=======
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
@@ -68,4 +38,3 @@ const campaignSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Campaign', campaignSchema);
->>>>>>> d0adf6f0bb198a25d1e18b1388978d580228e249
